8370110f2af8c48c6c06a3e3715c1724e63664c4
Giorgio Sironi <info@giorgiosironi.com>
2016-02-14 10:30:04 +0100

    Using .filter() and .map()

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,21 @@
 "use strict";
 
-// TODO: should I hide these functions inside a IIFE?
 var multipleOf = function(factor, number) {
   return number % factor == 0;
 };
 
 var FizzBuzz = function(number) {
-  // TODO: objects may not maintain order of keys, right?
   var mappings = [
     { factor: 3, word: "Fizz" },
     { factor: 5, word: "Buzz" }
   ];
-  var words = [];
-  // TODO: .map?
-  for (var i in mappings) {
-    if (multipleOf(mappings[i].factor, number)) {
-      words.push(mappings[i].word);
-    }
-  }
+  var words = mappings
+    .filter(function(mapping) {
+      return multipleOf(mapping.factor, number);
+    })
+    .map(function(mapping) {
+      return mapping.word;
+    });
   if (words.length > 0) {
     return words.join("");
   } else {
